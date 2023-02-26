0023bfc88777b676df8e462bc127453b4c86c6c5
James Runtas <jamesruntas@cmail.carleton.ca>
2023-02-26 18:43:44 -0500

    second

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,14 +6,14 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Cyber Security",
+          "Machine Learning",
+          "Systems Administration & DevOps",
+          "Software Development",
         ],
         autoStart: true,
         loop: true,
-        deleteSpeed: 50,
+        deleteSpeed: 100,
       }}
     />
   );
