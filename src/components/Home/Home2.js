363138e1cd4539d39bd2426ba0dcfb393c249caa
james <71133703+jamesruntas@users.noreply.github.com>
2023-03-13 12:19:51 -0400

    hackerrank

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -9,6 +9,9 @@ import {
   AiFillMessage,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
+import {
+  SiHackerrank
+} from "react-icons/si";
 
 function Home2() {
   return (
@@ -46,6 +49,16 @@ function Home2() {
                   className="icon-colour  home-social-icons"
                 >
                   <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.hackerrank.com/james_runtas"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <SiHackerrank />
                 </a>
               </li>
               <li className="social-icons">
