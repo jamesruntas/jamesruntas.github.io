dd40182e03312c0bdcaf8b10d6477e94d4449cee
james <jamesruntas@cmail.carleton.ca>
2026-03-11 21:37:27 -0400

    fix tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  //integrations: [tailwind()],
   output: 'static',
-  site: 'https://jamesruntas.github.io', // 🔁 replace with your GitHub username
+
+  // 🔁 replace with your GitHub username
+  site: 'https://jamesruntas.github.io',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
