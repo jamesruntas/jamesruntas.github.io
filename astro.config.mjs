8e764e0ecc3e7b3e91e14908f507faa0ade939e3
james <jamesruntas@cmail.carleton.ca>
2026-03-11 21:27:30 -0400

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-  site: 'https://yourusername.github.io', // 🔁 replace with your GitHub username
+  site: 'https://jamesruntas.github.io', // 🔁 replace with your GitHub username
 });
